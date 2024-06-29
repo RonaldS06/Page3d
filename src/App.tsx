a714a4da2da66d6c0f6f89ecce94572c151fc607
Ronald Santamaria <pizarroronald06@gmail.com>
2024-06-29 20:39:12 -0300

    Page v1

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Home from "./pages/home/Home";
+import ImgGallery from "./pages/gallery/ImgGallery";
+
+function App() {
+  return (
+    <>
+      <Home />
+      <ImgGallery />
+    </>
+  );
+}
+
+export default App;
