96541fbd99a9e8421056be45ee89925a4566017a
Ronald Santamaria <pizarroronald06@gmail.com>
2024-06-29 20:50:26 -0300

    PageDist

--- src/pages/home/Globe.tsx
+++ src/pages/home/Globe.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useAnimations, useGLTF } from "@react-three/drei";
 
 useGLTF.preload("/nasa_earth.glb");
 export default function Globe() {
   const group = useRef(null);
-  const { nodes, materials, animations, scene } = useGLTF("/nasa_earth.glb");
-  const { actions, clips } = useAnimations(animations, scene);
+  const { animations, scene } = useGLTF("/nasa_earth.glb");
+  const { actions } = useAnimations(animations, scene);
 
   useEffect(() => {
     console.log(actions);
